b9d2eb05b914d1f438283e3f6ad8e04cc5e3550e
MC探索者i <mcseekeri@outlook.com>
2021-02-07 12:43:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,21 +5,18 @@ window.Config = {
   SiteName: 'MC探索者i服务可用性检测',
 
   // 站点链接
-  SiteUrl: '/status/',
+  SiteUrl: 'status.mcseekeri.top',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
     'm785801886-fe798056dcefa062ca9f5619',
-    'm785802120-51f2dbd4221a7c635d514405',
     'm785802121-f726e102d93b206615b80ae4',
     'm785804188-4c7493bd8a273c86035ca35d',
-    'm785804192-aa17817f92d6d483eb38b016',
-    'm785804196-6102d834a82c0329dc733f91'
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: false,
+  ShowLink: true,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
@@ -28,6 +25,10 @@ window.Config = {
 
   // 导航栏菜单
   Navi: [
+    {
+      text: '资源合集',
+      url: 'https://sg.mcseekeri.top/'
+    }
     {
       text: '回到主页',
       url: 'https://mcseekeri.top/'
