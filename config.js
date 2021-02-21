39dba817908ab805d1d6e51de0a1891ca6299c48
MCSeekeri <mcseekeri@outlook.com>
2021-02-22 07:39:50 +0800

    90天好耶

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+const newLocal = 60;
 // 配置
 window.Config = {
 
@@ -27,7 +28,7 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 90,
 
   // 导航栏菜单
   Navi: [
